cd0e9dc86e899c92f5155ead623a40e07168a992
Jess Pates <jesspates@hotmail.co.uk>
2023-10-18 21:20:45 +0100

    navbar link underline slide effect

--- src/Layout/Nav.js
+++ src/Layout/Nav.js
@@ -1,16 +1,42 @@
-import classes from "./Nav.module.css";
+import { useState } from "react";
 import { Link } from "react-scroll";
+import { motion } from "framer-motion";
+import classes from "./Nav.module.css";
 
 function Nav() {
+  const [selectedLink, setSelectedLink] = useState("home");
+
+  function clickLinkHandler(event) {
+    setSelectedLink(event.target.innerText);
+  }
+
   return (
     <div className={classes["navbar__container"]}>
       <div className={classes["navbar__links"]}>
-        <Link to="home" spy={true} smooth={true} offset={-100} duration={800} className={classes["navbar__link"]}>home</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}className={classes["navbar__link"]}>projects</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}className={classes["navbar__link"]}>about</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}className={classes["navbar__link"]}>certificates</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}className={classes["navbar__link"]}>stack</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}className={classes["navbar__link"]}>contact</Link>
+        <div>
+          <Link to="home" layoutId="animate-link" spy={true} smooth={true} offset={-100} duration={800} onClick={clickLinkHandler} className={classes["navbar__link"]}>home</Link>
+          {selectedLink === "home" && <motion.div layoutId="animate-link" className={classes["navbar__link-underline"]}></motion.div>}
+        </div>
+        <div>
+          <Link to="projects" layoutId="animate-link" spy={true} smooth={true} offset={-100} duration={800} onClick={clickLinkHandler} className={classes["navbar__link"]}>projects</Link>
+          {selectedLink === "projects" && <motion.div layoutId="animate-link" className={classes["navbar__link-underline"]}></motion.div>}
+        </div>
+        <div>
+          <Link to="about" layoutId="animate-link" spy={true} smooth={true} offset={-100} duration={800} onClick={clickLinkHandler} className={classes["navbar__link"]}>about</Link>
+          {selectedLink === "about" && <motion.div layoutId="animate-link" className={classes["navbar__link-underline"]}></motion.div>}
+        </div>
+        <div>
+          <Link to="certificates" layoutId="animate-link" spy={true} smooth={true} offset={-100} duration={800} onClick={clickLinkHandler} className={classes["navbar__link"]}>certificates</Link>
+          {selectedLink === "certificates" && <motion.div layoutId="animate-link" className={classes["navbar__link-underline"]}></motion.div>}
+        </div>
+        <div>
+          <Link to="stack" layoutId="animate-link" spy={true} smooth={true} offset={-100} duration={800} onClick={clickLinkHandler} className={classes["navbar__link"]}>stack</Link>
+          {selectedLink === "stack" && <motion.div layoutId="animate-link" className={classes["navbar__link-underline"]}></motion.div>}
+        </div>
+        <div>
+          <Link to="contact" layoutId="animate-link" spy={true} smooth={true} offset={-100} duration={800} onClick={clickLinkHandler} className={classes["navbar__link"]}>contact</Link>
+          {selectedLink === "contact" && <motion.div layoutId="animate-link" className={classes["navbar__link-underline"]}></motion.div>}
+        </div>
       </div>
     </div>
   )
