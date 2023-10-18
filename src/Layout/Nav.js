28eee9e0897d64d3484593c536d75cc7689395ca
Jess Pates <jesspates@hotmail.co.uk>
2023-10-18 17:51:41 +0100

    sizing updates

--- src/Layout/Nav.js
+++ src/Layout/Nav.js
@@ -5,12 +5,12 @@ function Nav() {
   return (
     <div className={classes["navbar__container"]}>
       <div className={classes["navbar__links"]}>
-        <Link to="home" spy={true} smooth={true} offset={-100} duration={800}>home</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}>projects</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}>about</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}>certificates</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}>stack</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}>contact</Link>
+        <Link to="home" spy={true} smooth={true} offset={-100} duration={800} className={classes["navbar__link"]}>home</Link>
+        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}className={classes["navbar__link"]}>projects</Link>
+        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}className={classes["navbar__link"]}>about</Link>
+        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}className={classes["navbar__link"]}>certificates</Link>
+        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}className={classes["navbar__link"]}>stack</Link>
+        <Link to="projects" spy={true} smooth={true} offset={-100} duration={800}className={classes["navbar__link"]}>contact</Link>
       </div>
     </div>
   )
