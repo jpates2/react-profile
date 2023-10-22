968b4aab0c184d18e1e0f94b26d11b6f58140aa7
Jess Pates <jesspates@hotmail.co.uk>
2023-10-22 17:33:28 +0100

    certs expandable sections with framer motion

--- src/components/CertificateItem.js
+++ src/components/CertificateItem.js
@@ -1,10 +1,20 @@
-import classes from "./CertificateItem.module.css"
+import { AnimatePresence, motion } from 'framer-motion';
+import classes from "./CertificateItem.module.css";
 
 function CertificateItem(props) {
   return (
-    <div className={classes["cert-item"]}>
-      <div className={classes["cert-item__name"]}>{props.name}</div>
-      <div className={classes["cert-item__arrow"]}>	&#8964;</div>
+    <div className={classes["cert-item"]} onClick={props.onExpand} >
+      <div className={classes["cert-item__header"]} >
+        <div className={classes["cert-item__name"]}>{props.name}</div>
+        <motion.div animate={{rotate: props.isExpanded ? 180 : 0}} className={classes["cert-item__arrow"]}>	&#8964;</motion.div>
+      </div>
+      <AnimatePresence>
+        {props.isExpanded && (
+          <motion.div initial={{height: 0, opacity: 0}} animate={{height: "100px", opacity: 1}} exit={{height: 0, opacity: 0}} >
+            <p className={classes["cert-item__details"]}>Cert details here</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,26 +1,42 @@
+import { useState } from "react";
 import styles from "../styles/shared.module.css";
 import classes from "./Certificates.module.css"
 import CertificateItem from "./CertificateItem";
 
 const certificates = [
-  { name: "web development bootcamp" },
-  { name: "the complete javascript course" },
-  { name: "mastering typescript" },
-  { name: "css - the complete guide" },
-  { name: "tailwind from scratch" },
-  { name: "react - the complete guide" },
+  { id: "c1", name: "web development bootcamp" },
+  { id: "c2", name: "the complete javascript course" },
+  { id: "c3", name: "mastering typescript" },
+  { id: "c4", name: "css - the complete guide" },
+  { id: "c5", name: "tailwind from scratch" },
+  { id: "c6", name: "react - the complete guide" },
 ]
 
 function Certificates() {
+  const [expanded, setExpanded] = useState(null)
+
+  function handleExpand(id) {
+    setExpanded((prevId) => {
+      if (prevId === id) {
+        return null;
+      }
+      return id;
+    })
+  }
+
   const certsContent = certificates.map(cert => (
-    <CertificateItem key={cert.name} name={cert.name} />
+    <CertificateItem
+      key={cert.name}
+      onExpand={() => handleExpand(cert.id)}
+      isExpanded={expanded === cert.id}
+      name={cert.name} />
   ))
 
   return (
     <div>
       <h2 className={styles["section__title"]}>certificates</h2>
     <div className={classes["certificates__container"]}>
-      {certsContent}
+        {certsContent}
     </div>
     </div>
   )
