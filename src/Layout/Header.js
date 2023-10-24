1721047a97cb73d7139bdf34cfb550ca2e27d6c4
Jess Pates <jesspates@hotmail.co.uk>
2023-10-24 09:46:43 +0100

    stagger added to welcome header

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,27 +1,41 @@
 import { motion } from "framer-motion";
 import classes from "./Header.module.css"
 
+const listVariant = {
+  open: {
+    transition: {
+      staggerChildren: 0.2
+    }
+  }
+}
+
+const linkVariant = {
+  open: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: "easeOut"
+    }
+  }
+}
+
 function Header() {
   const header=["w", "e", "l", "c", "o", "m", "e"]
 
   return (
-    <div className={classes["header__container"]}>
-      <motion.div variants={{visible: {transition: {staggerChildren: 0.5}}}} className={classes["header__title"]} id="home">
+    <motion.div animate="open" className={classes["header__container"]}>
+      <motion.div variants={listVariant} className={classes["header__title"]} id="home">
         {header.map((letter, i) => (
           <motion.span
             key={`${letter} ${i}`}
-            variants={{
-              hidden: {opacity: 0, scale: 0.5},
-              visible: {opacity: 1, scale: [0.8, 1.3, 1]}
-            }}
-            exit={{opacity: 1, scale: 1}}
-            transition={{type: "spring"}}
+            initial={{opacity: 0}}
+            variants={linkVariant}
           >
             {letter}
           </motion.span>
         ))}
       </motion.div>
-    </div>
+    </motion.div>
   )
 }
 
